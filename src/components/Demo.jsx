40635c5d003b401ccf1e76c67ddef075fc60f796
An Ho <41894751+hothaian@users.noreply.github.com>
2024-03-20 23:02:10 -0700

    input textbox can take url and return console message with summary

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import {  copy, linkIcon, loader, tick } from "../assets";
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({
@@ -8,13 +8,39 @@ const Demo = () => {
     summary: "",
   });
 
-   const handleSubmit = async (e) => {
-    alert('Submitted')
-   }
+  const [allArticles, setAllArticles] = useState("");
 
-   const handleKeyDown = () => {
-    
-   }
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articleFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+    if (articleFromLocalStorage) {
+      setAllArticles(articleFromLocalStorage);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      console.log("🚀 ~ handleSubmit ~ newArticle:", newArticle);
+      console.log(
+        "🚀 ~ handleSubmit ~ updatedAllArticles:",
+        updatedAllArticles
+      );
+    }
+  };
+
+  const handleKeyDown = () => {};
 
   return (
     <section className="mt-16 w-full max-w-xl">
